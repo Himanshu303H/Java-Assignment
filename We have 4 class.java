fd5e3b2f69560a379acd9b807906c1334abb11d9
Himanshu303H <143948044+Himanshu303H@users.noreply.github.com>
2023-11-03 15:54:32 +0530

    Add files via upload

---
+++ We have 4 class.java
@@ -0,0 +1,48 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+//we will have 4 classes 
+class A{
+    public void series(int n1,int n2,int n){
+        for(int i=n1;i<=n2;i++){
+            if(n==0){
+                if(i%2==0){
+                    System.out.println(i+" ");
+                }
+            }
+            else{
+                if(i%2!=0){
+                    System.out.println(i+" ");
+                }
+            }
+        }
+    }
+}
+
+class B extends Thread{
+    public void run(){
+        A a =new A();
+        a.series(1,50,0);
+    }
+}
+
+class C extends Thread{
+    public void run(){
+        A a =new A();
+        a.series(1,50,1);
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+		B b = new B();
+		C c = new C();
+		b.start();
+		c.start();
+	}
+}
