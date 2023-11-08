6b71b1a5c7686b8de95f83b12122766ca038551c
Himanshu303H <143948044+Himanshu303H@users.noreply.github.com>
2023-11-08 11:51:24 +0530

    Add files via upload

---
+++ Generic method.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class ex{
+    public void printArray(String[] s){
+        for(int i=0;i<s.length;i++){
+            System.out.println(s[i]);
+        }
+    }
+        public static void main(String args[]){
+            ex e = new ex();
+            String []s1 = {"john","john"};
+            e.printArray(s1);
+            //Integer[]i={1,2,3,4};
+            //e.printArray(i);
+        }
+}
